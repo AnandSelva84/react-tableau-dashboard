06b5c4bfe366ae866e883c4d954782668b130360
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2020-05-08 16:51:59 +0300

    coming soon

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../loading/laoding.css";
+import "./home.css";
 
-const HomePage = (props) => <div>Home Page</div>;
+const HomePage = (props) => <div className="home">Coming Soon...</div>;
 
 export default HomePage;
