67f75aee6978c7f8f41e906d0d29d20b172c2405
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2020-05-13 16:21:52 +0300

    new filters json update

---
+++ server/src/db/newFiltersResponse.js
@@ -0,0 +1,51 @@
+const newFiltersResponse = {
+  filters: [
+    {
+      level: 0,
+      filterId: "Hierarchies",
+      title: "Hierarchies",
+      filterType: "Dropdwon",
+      parentFilterId: null,
+      values: [
+        {
+          filterOptionId: "legcy",
+          filter_value_text: "legcy",
+          filter_display_text: "legcy",
+          order: 1,
+          parentFilterOptionId: null,
+        },
+        {
+          filterOptionId: "Business",
+          filter_value_text: "Business",
+          filter_display_text: "Business",
+          order: 2,
+          parentFilterOptionId: null,
+        },
+      ],
+    },
+    {
+      level: 1,
+      filterId: "COSA",
+      title: "COSA",
+      filterType: "Multi-Select",
+      parentFilterId: "Hierarchies",
+      values: [
+        {
+          filterOptionId: "Bank1",
+          filter_value_text: "Bank",
+          filter_display_text: "Bank",
+          order: 1,
+          parentFilterOptionId: "Business",
+        },
+        {
+          filterOptionId: "Business Of IT",
+          filter_value_text: "Business Of IT",
+          filter_display_text: "Business Of IT",
+          order: 2,
+          parentFilterOptionId: "Business",
+        },
+      ],
+    },
+  ],
+};
+module.exports = { newFiltersResponse };
--- server/src/routes/filters.js
+++ server/src/routes/filters.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const Router = express.Router();
 const { ampInfo, kidInfo } = require("../db/infoResponses");
-const { filterResponse } = require("../db/filterResoinse");
+const { newFiltersResponse } = require("../db/newFiltersResponse");
 
 Router.get("/", (req, res) => {
   res.send({ ...filterResponse });
+});
+
+Router.get("/new", (req, res) => {
+  res.send({ ...newFiltersResponse });
 });
 
 module.exports = Router;
