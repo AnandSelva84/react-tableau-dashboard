90f81d1f6cda166475afcb399b73c71f84fe2c30
Anand Selvaraj <sanand.net@gmail.com>
2020-05-25 17:24:07 -0500

    react splitter layout package

--- client/src/pages/lvl-3/lvl-3.jsx
+++ client/src/pages/lvl-3/lvl-3.jsx
@@ -2,19 +2,17 @@ import React from "react";
 import "./lvl-3.css";
 // import SplitPane  from "react-split-pane";
 import Button from "../../components/button/button";
-import SplitPane from "react-split-pane";
+//import SplitPane from "react-split-pane";
+import SplitterLayout from 'react-splitter-layout';
+import 'react-splitter-layout/lib/index.css';
 
 const LVL_3 = (props) => {
   return (
     <>
-      <SplitPane split="horizontal" minSize={50} defaultSize={100}>
-        {/* <div className="wrapper">
-          <Button title="show table" />
-        </div>
-        <div className="wrapper"></div> */}
-        {/* <Pane className="">hello world</Pane>
-        <Pane className="">another one</Pane> */}
-      </SplitPane>
+       <SplitterLayout vertical>
+            <div>This is a div</div>
+            <div>This is a div</div>
+        </SplitterLayout> 
     </>
   );
 };
