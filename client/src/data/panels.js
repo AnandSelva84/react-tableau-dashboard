c4d157f116e49c145694f25e5f6c7c323ab63068
Anand Selvaraj <sanand.net@gmail.com>
2020-06-23 12:21:19 -0500

    panels renamed

--- client/src/data/panels.js
+++ client/src/data/panels.js
@@ -1,149 +1,50 @@
 export const panels = [
   {
-    title: "Effectivness (KPIs)",
-    route: 'effectivness',
-    level: 2,
-    items: [
-      {
-        title: "Story Delivery Lead Time",
-        route: 'storyDeliveryTime',
-        level: 3
-      },
-      {
-        title: "Feature Delivery Lead Time",
-        route: 'featureDeliveryLeadTime',
-        level: 3
-      },
-      {
-        title: "Sprint Predictability/Volatility",
-        route: 'sprintPredict',
-        level: 3
-      },
-      ,
-      {
-        title: "PI Predictability/Volatility",
-        route: 'piPredict',
-        level: 3
-      },
-      {
-        title: "High & Medium Defect Backlog",
-        route: 'defectBacklog',
-        level: 3
-      },
-      {
-        title: "New High Defects",
-        route: 'newDefects',
-        level: 3
-      }
+    title: "Effectivness",
+    innerValues: [
+      "Story Delivery Lead Time",
+      "Feature Delivery Lead Time",
+      "New Heigh Defects",
     ],
   },
-
   {
-    title: "Team-level Diagnostics",
-    route: 'teamDiagnostics',
-    level: 2,
-    items: [
-      {
-        title: "Story Cycle Time",
-        route: 'storyCycleTime',
-        level: 3
-      },
-      {
-        title: "Story Single Cycle Probability",
-        route: 'featureFirstCycle',
-        level: 3
-      },
-      {
-        title: "Story Latency (Segmented)",
-        route: 'storyLatency',
-        level: 3
-      },
-      ,
-      {
-        title: "Story Total Lead Time",
-        route: 'storyLatency',
-        level: 3
-      },
-      {
-        title: "Story Throughput",
-        route: 'storyThroughput',
-        level: 3
-      }
-    ]
+    title: "Team Level Diagnostics",
+    innerValues: ["Story Cycle Time", "Story Latency"],
   },
-
   {
-    title: "Program-level Diagnostics",
-    route: 'ProgramDiagnostics',
-    level: 2,
-    items: [
-      {
-        title: "Feature Cycle Time",
-        route: 'featureCycleTime',
-        level: 3
-      },
-      {
-        title: "Feature Total Lead Time",
-        route: 'featureLeadTime',
-        level: 3
-      },
-      {
-        title: "Feature Latency (Segmented)",
-        route: 'featureLatency',
-        level: 3
-      },
-      ,
-      {
-        title: "Feature Throughput",
-        route: 'featureThroughput',
-        level: 3
-      },
-      {
-        title: "Feature Completion on First Cycle",
-        route: 'featureFirstCycle',
-        level: 3
-      }
-    ]
+    title: "Quality",
+    innerValues: [
+      "Story Delivery Lead Time",
+      "Feature Delivery Lead Time",
+      "New Heigh Defects",
+    ],
   },
-
   {
-    title: "Work Tracking and Forecasting",
-    route: 'workTracking',
-    level: 2,
-    items: [
-      {
-        title: "Velocity",
-        route: 'storyVelocity',
-        level: 3
-      }
-    ]
+    title: "SDLC",
+    innerValues: ["Story Cycle Time", "Story Latency"],
   },
-
   {
-    title: "Census",
-    route: 'census',
-    level: 2,
-    items: [
-      {
-        title: "Team Count",
-        route: 'teamCount',
-        level: 3
-      },
-      {
-        title: "Team Size",
-        route: 'teamSize',
-        level: 3
-      },
-      {
-        title: "Team Locations",
-        route: 'teamLocations',
-        level: 3
-      },
-      {
-        title: "Team Members Mix",
-        route: 'teamMembers',
-        level: 3
-      }
-    ]
-  }
+    title: "Effectivness",
+    innerValues: [
+      "Story Delivery Lead Time",
+      "Feature Delivery Lead Time",
+      "New Heigh Defects",
+    ],
+  },
+  {
+    title: "Team Level Diagnostics",
+    innerValues: ["Story Cycle Time", "Story Latency"],
+  },
+  {
+    title: "Quality",
+    innerValues: [
+      "Story Delivery Lead Time",
+      "Feature Delivery Lead Time",
+      "New Heigh Defects",
+    ],
+  },
+  {
+    title: "SDLC",
+    innerValues: ["Story Cycle Time", "Story Latency"],
+  },
 ];
