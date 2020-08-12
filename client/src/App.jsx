5792b8a33dd0c4ce6aa943353b5c971e11bba025
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2020-08-12 15:32:10 +0300

    apply linting to app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,24 +2,13 @@ import React from "react";
 import Router from "./router/router";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { useLocation } from "react-router-dom";
-import useQuery from "./hooks/useQuery";
-// import { SnackbarProvider } from "notistack";
 
 function App() {
   return (
     <div style={{ height: "100%" }}>
-      {/* <SnackbarProvider
-        maxSnack={3}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "center",
-        }}
-      > */}
       <Provider store={store}>
         <Router />
       </Provider>
-      {/* </SnackbarProvider> */}
     </div>
   );
 }
